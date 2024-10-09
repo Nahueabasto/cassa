9650eeade56888d92515759bee3990bacb43a373
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2024-10-09 16:23:51 -0300

    fdsd

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,7 +7,7 @@ if (window.location.hostname === 'localhost') {
     baseURL = 'http://localhost:3000';
 } else {
     // Si no es localhost, asume que está en producción y utiliza la URL de producción
-    baseURL = 'https://cassa-psi.vercel.app/?vercelToolbarCode=3_qSWPWE1OdLGa6/';
+    baseURL = 'https://cassa-psi.vercel.app/';
 }
 
 // Crea una instancia de axios con la URL correspondiente
