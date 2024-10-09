7491ee5802f567b084d70fb7fd95e7bd0d9ed163
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2024-10-09 14:45:38 -0300

    f

--- src/component/Reservas/ContactoInfo.jsx
+++ src/component/Reservas/ContactoInfo.jsx
@@ -19,7 +19,7 @@ const ContactForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('http://localhost:3000/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
