a58d0f790510af6846ad2f2f7379c86df8d4923f
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2024-10-15 16:54:03 -0300

    f

--- src/component/Reservas/ReservasText.jsx
+++ src/component/Reservas/ReservasText.jsx
@@ -9,7 +9,7 @@ const ReservaTexto = () => {
       <h3>Abierto todo el año!</h3>
       <h3>Podés realizar tu reserva vía:</h3>
       <p><span class="bullet">•</span> Formulario de Contacto;</p>
-      <p><span class="bullet">•</span> Correo Electrónico (nahuel_1112@hotmail.com);</p>
+      <p><span class="bullet">•</span> Email (nahuel7474@gmail.com);</p>
       <h3>Hace tu reserva ya!</h3>
     </div>
   );
